e4c387e4aa9226b8a7f847f454892a0df5ce1c50
makuzya <103103196+makuzya@users.noreply.github.com>
2023-08-17 21:08:34 +0300

    Create script.js file

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let money = 5, 
+    income = 'Hello', 
+    addExpenses = {
+        a: 1,
+        b: 'js'
+    }, 
+    deposit = null, 
+    mission = [1, 2, 3], 
+    period = true;
+
+    console.log(addExpenses);
